271fad25ecc5c12a2bbe57435451a4838d869047
Nithish Saran <nithishsaran4050@gmail.com>
2025-04-08 10:19:47 +0530

    Initial commit of Salesflare Hugo site

---
+++ content/_index.md
@@ -0,0 +1,5 @@
+---
+title: "Salesflare - CRM Simplified"
+date: 2025-04-07
+---
+Welcome to Salesflare, the simple CRM for small businesses.
---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+
+	$('.open-menu').click(function(){
+		$('.sf-menu-mobile').slideDown('slow');;
+	});
+
+	$('.close-menu').click(function(){
+		$('.sf-menu-mobile').slideUp('slow');
+	});
+});
